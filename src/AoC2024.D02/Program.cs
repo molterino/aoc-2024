a4b9141b66296b697ad8b8f7919c33d3ee4c8c05
molterino <molti88@gmail.com>
2024-12-02 06:26:38 +0100

    add day 2 part 1 solution

---
+++ src/AoC2024.D02/Program.cs
@@ -0,0 +1,45 @@
+﻿const string path = "input.txt";
+const string separator = " ";
+
+try
+{
+    var safeLevels = 0;
+
+    foreach (var line in File.ReadAllLines(path))
+    {
+        var levels = line.Split(separator);
+        var increasingCount = 0;
+        var safeCount = 0;
+
+        for (int i = 0; i < levels.Length - 1; i++)
+        {
+            var currentLevel = int.Parse(levels[i]);
+            var nextLevel = int.Parse(levels[i + 1]);
+
+            var difference = Math.Abs(currentLevel - nextLevel);
+            if (difference >= 1 && difference <= 3)
+            {
+                safeCount++;
+            }
+
+            if (currentLevel > nextLevel)
+            {
+                increasingCount++;
+            }
+        }
+
+        var allIncreasingOrDecreasing = increasingCount == levels.Length - 1 || increasingCount == 0;
+        var allLevelSafe = safeCount == levels.Length - 1;
+
+        if (allIncreasingOrDecreasing && allLevelSafe)
+        {
+            safeLevels++;
+        }
+    }
+
+    Console.WriteLine($"Safe levels (part 1): {safeLevels}");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error during file processing: {e.Message}");
+}
