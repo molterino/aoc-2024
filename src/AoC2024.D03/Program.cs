b39d7b30c2babfa202e0dd7566d985eb29510f13
molterino <molti88@gmail.com>
2024-12-03 06:27:17 +0100

    add day 3 part 1 solution

---
+++ src/AoC2024.D03/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.RegularExpressions;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        const string path = "input.txt";
+
+        string pattern = @"mul\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)";
+        string memory = ReadMemory(path);
+        int result = 0;
+
+        foreach (Match match in Regex.Matches(memory, pattern))
+        {
+            var x = int.Parse(match.Groups[1].Value);
+            var y = int.Parse(match.Groups[2].Value);
+            var mul = x * y;
+            result += mul;
+        }
+
+        Console.WriteLine(result);
+    }
+
+    private static string ReadMemory(string path)
+    {
+        try
+        {
+            return File.ReadAllText(path);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error during file processing: {e.Message}");
+            return string.Empty;
+        }
+    }
+}
