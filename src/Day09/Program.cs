77b9ce47609c1af9b9f6251ef8298ad0f1c9d1f3
molterino <molti88@gmail.com>
2024-12-09 15:54:15 +0100

    add day 9 part 1 solution

---
+++ src/Day09/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Day09
+{
+    public static class Program
+    {
+        private const bool UseTemplateData = false;
+        private const string FreeSpace = ".";
+
+        private static void Main(string[] args)
+        {
+            var path = UseTemplateData ? "input-template.txt" : "input.txt";
+            var diskMap = File.ReadAllText(path);
+
+            var fileBlockId = 0;
+            var diskBlocks = new List<string>();
+
+            for(int i = 0; i < diskMap.Length; i++)
+            {
+                var blockLength = long.Parse(diskMap[i].ToString());
+                var block = FreeSpace;
+
+                if (i % 2 == 0) // even field - file, odd field - free space
+                {
+                    block = fileBlockId.ToString();
+                    fileBlockId++;
+                }
+
+                for (int j = 0; j < blockLength; j++)
+                {
+                    diskBlocks.Add(block);
+                }
+            }
+
+            // refragment
+            while (true)
+            {
+                var indexOfFirstEmptyBlock = diskBlocks.FindIndex(block => block == FreeSpace);
+                var indexOfLastFileBlock = diskBlocks.FindLastIndex(block => block != FreeSpace);
+
+                if (indexOfFirstEmptyBlock > indexOfLastFileBlock)
+                {
+                    break;
+                }
+
+                diskBlocks[indexOfFirstEmptyBlock] = diskBlocks[indexOfLastFileBlock];
+                diskBlocks[indexOfLastFileBlock] = FreeSpace;
+            }
+
+            // calculate filesystem checksum
+            long checksum = 0;
+
+            for (int i = 0; i < diskBlocks.FindIndex(x => x == FreeSpace); i++)
+            {
+                checksum += long.Parse(diskBlocks[i]) * i;
+            }
+
+            Console.WriteLine($"What is the resulting filesystem checksum? (Part1): {checksum}"); // 6432869891895
+        }
+    }
+}
