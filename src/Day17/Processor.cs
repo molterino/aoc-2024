beef28c69ab7b4eb44f37273b721a2e740b04627
molterino <molti88@gmail.com>
2025-01-05 22:13:32 +0100

    add day 17 part 2 solution

--- src/Day17/Processor.cs
+++ src/Day17/Processor.cs
@@ -2,43 +2,38 @@
 {
     public static class Processor
     {
-        private static int registerA = 0;
-        private static int registerB = 0;
-        private static int registerC = 0;
+        private static long registerA;
+        private static long registerB;
+        private static long registerC;
 
         private static List<int> instructions = [];
-        private static List<int> output = [];
+        private static List<long> output = [];
 
-        public static void InitRegisters(string path)
+        public static void Init(string[] data)
         {
-            var input = File.ReadAllLines(path);
-
-            registerA = int.Parse(input[0].Split(": ")[1]);
-            registerB = int.Parse(input[1].Split(": ")[1]);
-            registerC = int.Parse(input[2].Split(": ")[1]);
+            instructions = data[4].Split(": ")[1].Split(',').Select(int.Parse).ToList();
+            registerA = long.Parse(data[0].Split(": ")[1]);
+            registerB = long.Parse(data[1].Split(": ")[1]);
+            registerC = long.Parse(data[2].Split(": ")[1]);
+            output = [];
         }
 
-        public static void InitProgram(string path)
+        public static List<long> ExecuteProgram()
         {
-            var input = File.ReadAllLines(path);
+            output = [];
+            int instructionPointer = 0;
 
-            instructions = input[4].Split(": ")[1].Split(',').Select(int.Parse).ToList();
-        }
-
-        public static void ExecuteProgram()
-        {
-            var instructionPointer = 0;
             while (instructionPointer < instructions.Count)
             {
-                var opcode = instructions[instructionPointer];
-                var operandLiteral = instructions[instructionPointer + 1];
-                var operandCombo = CalculateOperandCombo(operandLiteral);
+                int opcode = instructions[instructionPointer];
+                int operandLiteral = instructions[instructionPointer + 1];
+                long operandCombo = CalculateOperandCombo(operandLiteral);
 
                 var shouldIncrementInstructionPointer = true;
                 switch (opcode)
                 {
                     case 0: // adv + combo
-                        registerA = (int)(registerA / Math.Pow(2, operandCombo));
+                        registerA = (long)(registerA / Math.Pow(2, operandCombo));
                         break;
                     case 1: // bxl + literal
                         registerB ^= operandLiteral;
@@ -60,10 +55,10 @@
                         output.Add(operandCombo % 8);
                         break;
                     case 6: // bdv - like adv
-                        registerB = (int)(registerA / Math.Pow(2, operandCombo));
+                        registerB = (long)(registerA / Math.Pow(2, operandCombo));
                         break;
                     case 7: // cdv - like adv
-                        registerC = (int)(registerA / Math.Pow(2, operandCombo));
+                        registerC = (long)(registerA / Math.Pow(2, operandCombo));
                         break;
                 }
 
@@ -72,19 +67,48 @@
                     instructionPointer += 2;
                 }
             }
+
+            return output;
+        }
+
+        public static long TryToCopyItself()
+        {
+            long registerValue = 0;
+            long upperBound = (long) Math.Pow(8, instructions.Count);
+
+            for (registerValue = 0; registerValue < upperBound; registerValue++)
+            {
+                SetRegisters(a: registerValue, b: 0, c: 0);
+                ExecuteProgram();
+
+                var hasSameLastDigits = CompareLastDigits();
+                if (hasSameLastDigits)
+                {
+                    var hasSameNumberOfDigits = output.Count == instructions.Count;
+                    if (hasSameNumberOfDigits)
+                    {
+                        break;
+                    }
+
+                    registerValue = (registerValue * 8) - 1;
+                }
+            }
+
+            return registerValue;
         }
 
         public static void PrintStatus()
         {
+            Console.WriteLine($"Instructions: {string.Join(',', instructions)}");
             Console.WriteLine($"A register value: {registerA}");
             Console.WriteLine($"B register value: {registerB}");
             Console.WriteLine($"C register value: {registerC}");
             Console.WriteLine($"Output: {string.Join(",", output)}");
         }
 
-        private static int CalculateOperandCombo(int operandLiteral)
+        private static long CalculateOperandCombo(long operandLiteral)
         {
-            var operandCombo = -1;
+            long operandCombo = -1;
 
             switch (operandLiteral)
             {
@@ -105,6 +129,31 @@
             }
 
             return operandCombo;
+        }
+
+        private static bool CompareLastDigits()
+        {
+            var startIndex = instructions.Count - output.Count;
+            var lastDigits = instructions.Skip(startIndex).ToList();
+            var isLastDigitsMatching = true;
+
+            for (int i = 0; i < output.Count; i++)
+            {
+                if (output[i] != lastDigits[i])
+                {
+                    isLastDigitsMatching = false;
+                    break;
+                }
+            }
+
+            return isLastDigitsMatching;
+        }
+
+        private static void SetRegisters(long a, long b, long c)
+        {
+            registerA = a;
+            registerB = b;
+            registerC = c;
         }
     }
 }
--- src/Day17/Program.cs
+++ src/Day17/Program.cs
@@ -4,14 +4,20 @@
     {
         private const string Path = "input.txt";
         //private const string Path = "input-template.txt";
+        //private const string Path = "input-template-2.txt";
 
         private static void Main(string[] args)
         {
-            Processor.InitRegisters(Path);
-            Processor.InitProgram(Path);
+            var data = File.ReadAllLines(Path);
+            Processor.Init(data);
 
-            Processor.ExecuteProgram();
-            Processor.PrintStatus(); // 2,0,7,3,0,3,1,3,7
+            // Part 1
+            var output = Processor.ExecuteProgram(); // 2,0,7,3,0,3,1,3,7
+            Console.WriteLine($"Output: {string.Join(',', output)}");
+
+            // Part 2
+            var registerA = Processor.TryToCopyItself(); // 247839539763386
+            Console.WriteLine($"RegisterA: {registerA}");
         }
     }
 }
